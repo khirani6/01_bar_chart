2a91fcba2fb70b9cfc4b2bdb91b67c113bb66c09
khirani6 <khirani6@gatech.edu>
2018-03-07 23:26:51 -0500

    Adding code to load csv data and manipulate it

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 
 // **** Your JavaScript code goes here ****
+/*
+
+Name: Komal Hirani
+gtID: khirani6
+CS 4460 - Coding Assignment #1
+
+*/
+
+d3.csv('./data/coffee_data.csv', function(error, data) {
+	if (error) {
+		console.error('Error loading coffee sale data');
+		console.error(error);
+		return;
+	}
+	
+	//console.log(data);
+	var salesByRegion = d3.nest()
+		.key(function(d) {
+			return d.region;
+		})
+		.rollup(function(v) {
+			var totalSales = d3.sum(v, function(d) {
+				return d.sales;
+			});
+			return totalSales;
+		})
+		.entries(data);
+
+	//console.log(JSON.stringify(salesByRegion));
+	console.log(salesByRegion);
+
+});
 
